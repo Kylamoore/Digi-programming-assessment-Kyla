c34790f2f163ce3179fc354a196868e56c81562e
Kylamoore <kylamoore@rhs.school.nz>
2026-09-07 14:47:41 +1200

    Add files via upload
    this version got the score counter working (and showing) and changed the size of things around

--- whac_a_mole.py
+++ whac_a_mole.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QGridLayout, QPushButton, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGridLayout, QPushButton, QWidget, QLabel, QVBoxLayout, QHBoxLayout
 import random
+from PyQt6.QtCore import QTimer
 
 class GameWindow(QMainWindow):
     def __init__(self):
@@ -8,10 +9,19 @@ class GameWindow(QMainWindow):
 
         # set window presets
         self.setWindowTitle ("Whac-A-Mole")
-        self.setFixedSize (500, 500)
+        self.setFixedSize (380, 400)
 
-        # make buttons
+        self.score_label = QLabel("Score: 0")
+        self.time_label = QLabel("60s")
+
+        heading_h_box = QHBoxLayout()
+        heading_h_box.addWidget(self.score_label)
+        heading_h_box.addStretch()
+        heading_h_box.addWidget(self.time_label)
+
+        # make buttons grid
         grid_layout = QGridLayout()
+        grid_layout.setSpacing(20)
         self.buttons = []
         for row in range(4):
             row_buttons = []
@@ -23,12 +33,20 @@ class GameWindow(QMainWindow):
                 button.clicked.connect(lambda clicked, r=row, c=col: self.button_clicked(r, c))
             self.buttons.append(row_buttons)
 
-        # create grid
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(heading_h_box)
+        main_layout.addLayout(grid_layout)
+        main_layout.addStretch()
+
+        # create central
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-        central_widget.setLayout(grid_layout)
+        central_widget.setLayout(main_layout)
 
+        # starting score and mole
+        self.player_score_counter = 0
         self.mole_status = None
+        # start first mole
         self.add_mole()
 
     # add mole when all the buttons are blank
@@ -46,10 +64,19 @@ class GameWindow(QMainWindow):
     # button actions
     def button_clicked(self, row, col):
         self.clicked_button = self.buttons[row][col]
+        # if the button that was clicked is actually a mole, set it blank and add point
         if (row, col) == self.mole_status:
             self.buttons[row][col].setText("")
             self.mole_status = None
+            self.player_score()
             self.add_mole()
+
+    # add a point to the players score
+    def player_score(self):
+        self.player_score_counter = self.player_score_counter + 1
+        print (self.player_score_counter)
+        self.score_label.setText(f"Score: {self.player_score_counter}")
+
 
 # start game
 app = QApplication(sys.argv)
